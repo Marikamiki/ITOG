2afd175a7852cdabed1ffd708c386906e39c0cf6
Marikamiki <118117702+Marikamiki@users.noreply.github.com>
2023-01-15 14:41:15 +0300

    added whole ex

---
+++ ITOG/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символам
+
+Console.Clear();
+
+string[] M;
+Console.WriteLine("Enter massiv with blankspaces");
+
+string line = Console.ReadLine();
+M = line.Split(' ');
+var result = new string[M .Length];
+var realSize = 0;
+foreach (var value in M )
+{
+    if (value.Length <= 3)
+         {
+            result[realSize] = value;
+            realSize++;
+        }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+
+Console.ReadKey(true);
